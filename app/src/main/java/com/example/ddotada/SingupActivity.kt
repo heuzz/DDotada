590d5ecd7f0949e6c4795a3016585728c0f91aaa
heuzz <69620444+heuzz@users.noreply.github.com>
2021-07-02 17:00:42 +0900

    회원가입 화면 생성

--- app/src/main/java/com/example/ddotada/SingupActivity.kt
+++ app/src/main/java/com/example/ddotada/SingupActivity.kt
@@ -3,9 +3,9 @@ package com.example.ddotada
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class menuscreen_activity : AppCompatActivity() {
+class SingupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menuscreen)
+        setContentView(R.layout.activity_singup)
     }
 }
