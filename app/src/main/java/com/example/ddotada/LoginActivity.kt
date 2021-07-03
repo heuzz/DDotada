b4ea3bb3b69d8349a2e54d955c1b9ab149da6849
heuzz <69620444+heuzz@users.noreply.github.com>
2021-07-03 22:24:03 +0900

    커밋

--- app/src/main/java/com/example/ddotada/LoginActivity.kt
+++ app/src/main/java/com/example/ddotada/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.example.ddotada
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn_signup = findViewById<ImageButton>(R.id.btn_signup)
+
+        btn_signup.setOnClickListener {
+
+        }
     }
 }
