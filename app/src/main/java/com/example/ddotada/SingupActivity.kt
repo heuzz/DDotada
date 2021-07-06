cf149635a4ef445842e447654d702724c860377d
heuzz <69620444+heuzz@users.noreply.github.com>
2021-07-06 10:10:07 +0900

    데이터베이스 추가, 써클뷰 추가

--- app/src/main/java/com/example/ddotada/SingupActivity.kt
+++ app/src/main/java/com/example/ddotada/SingupActivity.kt
@@ -6,9 +6,13 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SingupActivity : AppCompatActivity() {
 
+    private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val databaseReference: DatabaseReference = firebaseDatabase.getReference()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +21,8 @@ class SingupActivity : AppCompatActivity() {
 
         //변수 초기화
         val edit_nickname = findViewById<EditText>(R.id.edit_nickname)
-        val edit_name = findViewById<EditText>(R.id.edit_id)
+        val edit_name = findViewById<EditText>(R.id.edit_name)
+        val edit_id = findViewById<EditText>(R.id.edit_id)
         val edit_phone = findViewById<EditText>(R.id.edit_phone)
         val edit_password = findViewById<EditText>(R.id.edit_password)
         val edit_passwordcheck = findViewById<EditText>(R.id.edit_passwordcheck)
@@ -29,6 +34,7 @@ class SingupActivity : AppCompatActivity() {
         var phone:String? = null
         var password:String? = null
         var passwordcheck:String? = null
+        var id:String? = null
         var error : Int = 0
 
         btn_change_profile.setOnClickListener {
@@ -38,10 +44,11 @@ class SingupActivity : AppCompatActivity() {
         btn_signup2.setOnClickListener {
             nickname = edit_nickname.text.toString()
             name = edit_name.text.toString()
+            id = edit_id.text.toString()
             phone = edit_phone.text.toString()
             password = edit_password.text.toString()
             passwordcheck = edit_passwordcheck.text.toString()
-            if(nickname == "" || name == "" || phone == "" || password == "" || password != passwordcheck){
+            if(nickname == "" || id == "" ||name == "" || phone == "" || password == "" || password != passwordcheck){
                 error = 1
             }
             else{
@@ -51,6 +58,9 @@ class SingupActivity : AppCompatActivity() {
                 when {
                     nickname == "" -> {
                         Toast.makeText(this, "닉네임을 작성하지 않았습니다.",Toast.LENGTH_SHORT).show()
+                    }
+                    id == ""-> {
+                        Toast.makeText(this,"아이디를 작성하지 않았습니다.",Toast.LENGTH_SHORT).show()
                     }
                     name == "" -> {
                         Toast.makeText(this,"이름을 작성하지 않았습니다.",Toast.LENGTH_SHORT).show()
@@ -67,6 +77,10 @@ class SingupActivity : AppCompatActivity() {
                 }
             }
             else {
+                databaseReference.child("User").child("${id}").child("id").setValue("${id}")
+                databaseReference.child("User").child("${id}").child("nickname").setValue("${nickname}")
+                databaseReference.child("User").child("${id}").child("phone").setValue("${phone}")
+                databaseReference.child("User").child("${id}").child("pw").setValue("${password}")
                 Toast.makeText(this,"회원가입이 되었습니다.",Toast.LENGTH_SHORT).show()
                 val intent = Intent(this,LoginActivity::class.java)
                 startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
