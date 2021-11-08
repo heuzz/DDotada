6ce0ba33ada87bfedbd0c8a88f30af79a926b14d
heuzz <69620444+heuzz@users.noreply.github.com>
2021-11-08 14:54:44 +0900

    반영 전 수정

---
+++ app/src/main/java/com/example/ddotada/precaution_before_starting2.kt
@@ -0,0 +1,11 @@
+package com.example.ddotada
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class precaution_before_starting2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_precaution_before_starting2)
+    }
+}
